3f938ed1a603a336b51c1e192ed6c520d459368f
Philip Moore <prmoore77@hotmail.com>
2022-08-18 14:57:22 -0400

    Minor adjustments

--- tpc-h_queries/h13.sql
+++ tpc-h_queries/h13.sql
@@ -6,7 +6,7 @@ FROM
    FROM customer
    LEFT OUTER JOIN orders ON c_custkey = o_custkey
    AND o_comment NOT LIKE '%special%requests%'
-   GROUP BY c_custkey)
+   GROUP BY c_custkey) AS t
 GROUP BY c_count
 ORDER BY custdist DESC,
          c_count DESC
