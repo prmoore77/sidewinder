f031c9df688683b005f0666e30d29907408cd454
Philip Moore <prmoore77@hotmail.com>
2022-09-06 13:53:26 -0400

    Added parallel gunzip for worker.  Added timestamp to log format.  Added star query example.
    Former-commit-id: d251ca5975fb370e5c8bfced4624ee48a9513e2a

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ import sys
 import logging
 
 # Setup logging
-logging.basicConfig(level=getattr(logging, os.getenv("LOG_LEVEL", "INFO")),
+logging.basicConfig(format='%(asctime)s - %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S %Z',
+                    level=getattr(logging, os.getenv("LOG_LEVEL", "INFO")),
                     stream=sys.stdout
                     )
 
